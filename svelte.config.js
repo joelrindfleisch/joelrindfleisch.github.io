053e7a9a40f430131ff65c0782dc3fdd6d09ca5f
joelrindfleisch <joelrindfleisch@gmail.com>
2022-04-24 11:52:45 -0500

    fix: base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
+const dev = process.env.NODE_ENV === 'local';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,7 +15,7 @@ const config = {
 			fallback: 'index.html'
 		}),
 		paths: {
-			base: dev ? '' : '/joelrindfleisch.github.io',
+			base: dev ? '' : 'joelrindfleisch.github.io',
 		}
 	}
 };
